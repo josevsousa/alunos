10f224a20a3611fd7ca5f9526f00c4347b918fc2
josevsousa <jose.vicente.de.sousa@gmail.com>
2023-09-18 22:24:35 -0300

    implementação do auth firebase google ok

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,23 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideAnimations } from '@angular/platform-browser/animations';
 
+// firebase auth
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { enviroment } from './enviroments/enviroment';
+
+
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimations()]
+  providers: [
+    provideRouter(routes), 
+    provideAnimations(),
+    importProvidersFrom(
+      AngularFireModule.initializeApp(enviroment.firebase),
+      AngularFireAuthModule
+    )
+  ]
 };
--- src/app/tela-login/tela-login.component.ts
+++ src/app/tela-login/tela-login.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {MatInputModule} from '@angular/material/input';
 import {MatButtonModule} from '@angular/material/button';
@@ -6,6 +6,9 @@ import {MatIconModule} from '@angular/material/icon';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { GoogleAuthProvider } from '@angular/fire/auth';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-tela-login',
@@ -23,14 +26,36 @@ import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 })
 export default class TelaLoginComponent {
 
+  user$ = this.angularAuth.authState.pipe(
+    map(user => ({user}))
+  )
+
   loginForm = new FormGroup({
     usuario: new FormControl(''),
     senha: new FormControl('')
   })
 
+  constructor( private angularAuth: AngularFireAuth ){}
+
+
+
   ddd() {
     console.log(this.loginForm.value)
   }
+
+
+  loginGoogle(){
+    this.angularAuth.signInWithPopup(new GoogleAuthProvider())
+      .then(()=>{
+        console.log(this.user$)
+      })
+      .catch((error)=> console.log(error))
+  }
+
+  desconectarGoogle(){
+    this.angularAuth.signOut();
+  } 
+
 
 
 }
